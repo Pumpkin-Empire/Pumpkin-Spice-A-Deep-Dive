89b4be5c41a74d9fe32e1e44eee35b1a5562cf8c
Nick Nilson <nilson.nicholas@gmail.com>
2022-08-26 12:48:34 -0400

    some reformatting, and updating api call function to return more tweets

--- PumpkinEmpire.py
+++ PumpkinEmpire.py
@@ -1,7 +1,7 @@
-import json
+# import json
 import requests
 import config
-import pandas as pd
+# import pandas as pd
 from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -12,10 +12,7 @@ dbname = config.dbname
 uname = config.uname
 pwd = config.pwd
 
-# engine = create_engine('postgresql://scott:tiger@localhost:5432/mydatabase')
-engine = create_engine("postgresql://{user}:{pw}@{host}/{db}".format(host=hostname, db=dbname, user=uname,pw=pwd))
-
-
+engine = create_engine("postgresql://{user}:{pw}@{host}/{db}".format(host=hostname, db=dbname, user=uname, pw=pwd))
 
 
 def get_date_string() -> str:
@@ -31,8 +28,8 @@ def auth():
 
 
 def create_headers(bearer_token) -> dict:
-    headers = {"Authorization": "Bearer {}".format(bearer_token)}
-    return headers
+    header = {"Authorization": "Bearer {}".format(bearer_token)}
+    return header
 
 
 def create_url(max_results=10) -> tuple:
@@ -48,17 +45,13 @@ def create_url(max_results=10) -> tuple:
     return search_url, query_params
 
 
-def connect_to_api(url, headers, params, next_token = None):
+def connect_to_api(url, headers, params, next_token=None):
     params['next_token'] = next_token
-    response = requests.request("GET", url, headers = headers, params = params)
+    response = requests.request("GET", url, headers=headers, params=params)
     print("Endpoint Response Code: " + str(response.status_code))
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
-
-bearer_token = config.bearer_token
-headers = create_headers(bearer_token)
-url = create_url()
 
 
 def append_dict_values(base_dict: dict, append_dict: dict) -> dict:
@@ -79,15 +72,12 @@ def append_dict_values(base_dict: dict, append_dict: dict) -> dict:
 def loop_connect() -> dict:
 
     # May be able to make these global, depending on the automation used later.
-    max_requests_per_call = 10
-    max_requests_per_window = 20
+    max_requests_per_call = 100
+    max_requests_per_window = 180
     url = create_url(max_requests_per_call)
 
     # Get Initial Response
     json_response = connect_to_api(url[0], headers, url[1])
-
-    # Store list of response keys for appending dict values later.
-    response_keys = list(json_response.keys())
 
     # Update how many items we can request
     requests_per_call = max_requests_per_window - max_requests_per_call
@@ -97,8 +87,6 @@ def loop_connect() -> dict:
         json_response = append_dict_values(json_response, temp_response)
         requests_per_call = requests_per_call - max_requests_per_call
     return json_response
-
-
 
 
 def add_tweets_to_db(response: dict):
@@ -140,14 +128,18 @@ def add_tweets_to_db(response: dict):
 # print(type(json_response))
 # print(json.dumps(json_response, indent=4, sort_keys=True))
 
-# dict1 = {'data': [{'user': 'roy', 'tweet': 'hey there friendo'}, {'user': 'kendra', 'tweet': 'nick get back to work'}], 'meta': [1, 2]}
-# dict2 = {'data': [{'user': 'bevin', 'tweet': 'not your friendo buddyo'}, {'user': 'kris', 'tweet': 'nick is MVP'}], 'meta': [3, 4]}
+# dict1 = {'data': [{'user': 'roy', 'tweet': 'hey there friendo'}, {'user': 'kendra', 'tweet': \
+#   'nick get back to work'}], 'meta': [1, 2]}
+# dict2 = {'data': [{'user': 'bevin', 'tweet': 'not your friendo buddyo'}, {'user': 'kris', \
+#   'tweet': 'nick is MVP'}], 'meta': [3, 4]}
 # print(append_dict_values(dict1, dict2))
 
 # print(loop_connect())
 # print(json.dumps(loop_connect(), indent=4, sort_keys=True))
 
 if __name__ == "__main__":
-
+    bearer_token = config.bearer_token
+    headers = create_headers(bearer_token)
+    url = create_url()
     response = loop_connect()
     add_tweets_to_db(response)
